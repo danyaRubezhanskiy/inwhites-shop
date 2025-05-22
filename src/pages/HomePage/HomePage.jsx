5c6c0b8d5788065e7c38999d494dd01e547cb839
danyaRubezhanskiy <danja.rubeghanskij@gmail.com>
2025-05-22 19:36:46 +0200

    hero section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header.jsx";
+import Hero from "../../components/Main/Hero.jsx";
 
 const HomePage = () => {
   return (
     <div>
       <Header></Header>
+      <Hero></Hero>
     </div>
   );
 };
