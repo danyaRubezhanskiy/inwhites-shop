d3600cb15abdedd49de750de72e5fbfad7dc49c0
danyaRubezhanskiy <danja.rubeghanskij@gmail.com>
2025-06-20 20:05:10 +0200

    reviews items

--- src/components/ProductReviews/ProductReviews.jsx
+++ src/components/ProductReviews/ProductReviews.jsx
@@ -1,10 +1,29 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import container from "../../container.module.css";
 import css from "./ProductReviews.module.css";
+import { Rate } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductReviews } from "../../store/Slices/reviewSlice";
 
 const ProductReviews = () => {
+  const dispatch = useDispatch();
+
+  const reviews = useSelector((state) => state.reviews.productReviews);
+
+  const [showAll, setShowAll] = useState(false);
+
+  const visibleProducts = showAll ? reviews : reviews.slice(0, 6);
+
+  useEffect(() => {
+    dispatch(getProductReviews());
+  }, [dispatch]);
+
+  const tooggleShow = () => {
+    setShowAll((prev) => !prev);
+  };
+
   return (
     <section className={clsx(container.container, css.section)}>
       <div className={css.reviewsAllDiv}>
@@ -17,6 +36,33 @@ const ProductReviews = () => {
           </svg>
           <button className={css.addReviewBtn}>Write a Review</button>
         </div>
+      </div>
+      <div className={css.reviewsSection}>
+        <ul className={css.reviewsList}>
+          {visibleProducts.map((review) => (
+            <li key={review.id} className={css.reviewsItem}>
+              <div className={css.reviewsContainer}>
+                <Rate defaultValue={5} disabled={true} allowHalf={true} />
+                <h3 className={css.reviewsName}>
+                  {review.name}
+                  <svg width="20" height="20">
+                    <use href="../../../../public/icons/Review.svg#galochka" />
+                  </svg>
+                </h3>
+                <p className={css.reviewsText}>{review.comment}</p>
+                <p className={css.reviewsDate}>{review.date}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button
+          onClick={() => {
+            tooggleShow();
+          }}
+          className={css.viewBtn}
+        >
+          {!showAll ? "Load More Reviews" : "Hide All"}
+        </button>
       </div>
     </section>
   );
--- src/store/Slices/reviewSlice.js
+++ src/store/Slices/reviewSlice.js
@@ -1,16 +1,29 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import reviews from "../reviews";
+import productReviews from "../../components/ProductReviews/productReviews";
 
 const initialState = {
   items: [],
+  productReviews: [],
   loading: false,
   error: null,
 };
 
-export const getReviews = createAsyncThunk("reviews/getReviews", async () => {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  return reviews;
-});
+export const getReviews = createAsyncThunk(
+  "reviews/getReviews",
+  async () => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return reviews;
+  }
+);
+
+export const getProductReviews = createAsyncThunk(
+  "reviews/getProductReviews",
+  async () => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return productReviews;
+  }
+);
 
 const reviewsSlice = createSlice({
   name: "reviews",
@@ -26,6 +39,17 @@ const reviewsSlice = createSlice({
         state.items = action.payload;
       })
       .addCase(getReviews.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(getProductReviews.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getProductReviews.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.productReviews = action.payload;
+      })
+      .addCase(getProductReviews.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
       });
